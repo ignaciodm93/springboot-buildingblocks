8b672897cbc1ff08aa17587510e9bb857bf90946
Ignacio Della Morte <ignaciodm93@gmail.com>
2021-01-22 12:06:09 -0300

    JPA Base RestServices Development Completed

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -1,7 +1,9 @@
 package com.stacksimplify.restservices;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 //
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
@@ -9,5 +11,11 @@ public class SpringbootBuildingblocksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
+	
+	
+	
 
+	
+	
+	
 }
