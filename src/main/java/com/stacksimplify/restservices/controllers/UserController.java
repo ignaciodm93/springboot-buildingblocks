a62035c893642d32ce1ad7f3bd0e8d70b376d2a5
Ignacio Della Morte <ignaciodm93@gmail.com>
2021-01-26 15:40:23 -0300

    Error Handling

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -3,10 +3,14 @@ package com.stacksimplify.restservices.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +24,11 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.stacksimplify.restservices.entities.User;
 import com.stacksimplify.restservices.exceptions.UserExistsException;
 import com.stacksimplify.restservices.exceptions.UserNotFoundException;
+import com.stacksimplify.restservices.exceptions.UsernameNotFoundException;
 import com.stacksimplify.restservices.services.UserService;
 
 @RestController
+@Validated
 public class UserController {
 
 	
@@ -40,7 +46,7 @@ public class UserController {
 	
 	//create user
 	@PostMapping("/users")
-	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder builder) {
+	public ResponseEntity<Void> createUser(@Valid @RequestBody User user, UriComponentsBuilder builder) {
 		
 		try {
 			 userService.createUser(user);
@@ -60,7 +66,7 @@ public class UserController {
 	
 	
 	@GetMapping("/users/{id}")
-	public Optional<User> getUserById(@PathVariable("id") Long id){
+	public Optional<User> getUserById(@PathVariable("id") @Min(1) Long id){
 		
 		try {
 			return userService.getUserById(id);	
@@ -91,10 +97,27 @@ public class UserController {
 	}
 	
 	
+	
+	
 	@GetMapping("/users/byusername/{username}")
-	public User getUserByUsername(@PathVariable("username") String username) {
-		return userService.getUserByUsername(username);
+	public User getUserByUsername(@PathVariable("username") String username) throws UsernameNotFoundException {
+		
+		User user = userService.getUserByUsername(username);
+		
+		if(user == null) {
+			throw new UsernameNotFoundException("Username '" + username + "' not founded in User repository");
+		}
+		
+		return user;
+		
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UsernameNotFoundException.java
@@ -0,0 +1,23 @@
+package com.stacksimplify.restservices.exceptions;
+
+
+
+
+public class UsernameNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//Superclass Constructor
+	public UsernameNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+}
